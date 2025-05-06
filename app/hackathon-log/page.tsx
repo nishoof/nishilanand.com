b0c15e9ae3117aa839e0ab7cafcf4897f8e60bf0
nishoof <88177986+nishoof@users.noreply.github.com>
2025-05-05 20:20:00 -0700

    Get Hackathon Log entries from list instead of hard coded + add links

--- app/hackathon-log/page.tsx
+++ app/hackathon-log/page.tsx
@@ -1,4 +1,67 @@
 import { BackButton } from "@/components/BackButton";
+import { ExternalLink } from "@/components/ExternalLink";
+
+interface Hackathon {
+  date: string;
+  location: string;
+  name: string;
+  prizes: string;
+  projectName: string;
+  projectLink: string;
+}
+
+const hackathons: Hackathon[] = [
+  {
+    date: "Apr 4-6, 2025",
+    location: "USF",
+    name: "DonsHack 25",
+    prizes: "Best Website (most competitive + prizes)",
+    projectName: "creg",
+    projectLink: "http://github.com/nishoof/creg",
+  },
+  {
+    date: "Mar 22, 2025",
+    location: "Cloudflare, SF",
+    name: "AI Agents Hackathon - GTC 2025 Edition",
+    prizes: "Best use of Tavus AI (3rd), Best use of Windsurf (2nd)",
+    projectName: "Launchpad",
+    projectLink: "http://github.com/nishoof/cloudflare-hack",
+  },
+  {
+    date: "Feb 28 - Mar 2, 2025",
+    location: "USF",
+    name: "BLOOM",
+    prizes: "1st Overall",
+    projectName: "crts.io",
+    projectLink: "http://github.com/nishoof/crts",
+  },
+  {
+    date: "Nov 8-10, 2024",
+    location: "USF",
+    name: "DEPLOY/24",
+    prizes: "3rd Overall",
+    projectName: "Newsflash",
+    projectLink: "http://github.com/nishoof/newsflash",
+  }
+]
+
+const UNICODE_NO_BREAK_SPACE = "\u00A0";
+
+function HackathonRow({ hackathon }: { hackathon: Hackathon }) {
+  return (
+    <tr key={hackathon.name}>
+      <td>{hackathon.date}</td>
+      <td>{hackathon.location}</td>
+      <td>{hackathon.name}</td>
+      <td>{hackathon.prizes}</td>
+      <td>
+        <ExternalLink href={hackathon.projectLink}>
+          {hackathon.projectName + UNICODE_NO_BREAK_SPACE + "↗"}
+        </ExternalLink>
+      </td>
+    </tr>
+  );
+}
 
 export default function HackathonLog() {
   return (
@@ -19,34 +82,9 @@ export default function HackathonLog() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Apr 4-6, 2025</td>
-              <td>USF</td>
-              <td>DonsHack 25</td>
-              <td>Best Website (most competitive + prizes) </td>
-              <td>creg</td>
-            </tr>
-            <tr>
-              <td>Mar 22, 2025</td>
-              <td>Cloudflare</td>
-              <td>AI Agents Hackathon - GTC 2025 Edition</td>
-              <td>Best use of Tavus AI (3rd), Best use of Windsurf (2nd)</td>
-              <td>Launchpad</td>
-            </tr>
-            <tr>
-              <td>Feb 28 - Mar 2, 2025</td>
-              <td>USF</td>
-              <td>BLOOM</td>
-              <td>1st Overall</td>
-              <td>crts.io</td>
-            </tr>
-            <tr>
-              <td>Nov 8-10, 2024</td>
-              <td>USF</td>
-              <td>DEPLOY/24</td>
-              <td>3rd Overall</td>
-              <td>Newsflash</td>
-            </tr>
+            {hackathons.map((hackathon, i) => (
+              <HackathonRow key={i} hackathon={hackathon} />
+            ))}
           </tbody>
         </table>
       </div>
